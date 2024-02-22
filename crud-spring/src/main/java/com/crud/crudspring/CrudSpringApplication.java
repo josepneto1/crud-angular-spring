1a602f0ed64166796d2828802a236df893f12499
josepneto <passagnolo1@gmail.com>
2024-02-22 06:58:01 -0300

    Validações na API

--- crud-spring/src/main/java/com/crud/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/crud/crudspring/CrudSpringApplication.java
@@ -21,7 +21,7 @@ public class CrudSpringApplication {
 
 			Curso c = new Curso();
 			c.setNome("Angular com Spring");
-			c.setCategoria("front-end");
+			c.setCategoria("Front-end");
 
 			cursoRepository.save(c);
 		};
--- crud-spring/src/main/java/com/crud/crudspring/controller/CursoController.java
+++ crud-spring/src/main/java/com/crud/crudspring/controller/CursoController.java
@@ -2,14 +2,19 @@ package com.crud.crudspring.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 // import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.crud.crudspring.model.Curso;
 import com.crud.crudspring.repository.CursoRepository;
 
+@Validated
 @RestController
 @RequestMapping("/api/cursos")
 //@AllArgsConstructor se usar não precisa se construtor
@@ -28,7 +33,7 @@ public class CursoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Curso> buscarPorId(@PathVariable Long id){
+    public ResponseEntity<Curso> buscarPorId(@PathVariable @NotNull @Positive Long id){
         return cursoRepository.findById(id)
                 .map(registro -> ResponseEntity.ok().body(registro))
                 .orElse(ResponseEntity.notFound().build());
@@ -37,12 +42,13 @@ public class CursoController {
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Curso criar(@RequestBody Curso curso){
+    public Curso criar(@RequestBody @Valid Curso curso){
         return cursoRepository.save(curso);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Curso> atualizar(@PathVariable Long id, @RequestBody Curso curso){
+    public ResponseEntity<Curso> atualizar(@PathVariable @NotNull @Positive Long id,
+                                           @RequestBody @Valid Curso curso){
         return cursoRepository.findById(id)
                 .map(registro -> {
                     registro.setNome(curso.getNome());
@@ -54,7 +60,7 @@ public class CursoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable Long id){
+    public ResponseEntity<Void> deletar(@PathVariable @NotNull @Positive Long id){
         return cursoRepository.findById(id)
                 .map(registro -> {
                     cursoRepository.deleteById(id);
